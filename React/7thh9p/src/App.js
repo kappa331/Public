384e29be827354ad3de7115acc7933c5dfa2f14e
kappa <wawata1qwerty@gmail.com>
2023-09-20 14:11:40 +0900

    finish tutorial 3

--- React/7thh9p/src/App.js
+++ React/7thh9p/src/App.js
@@ -1,8 +1,17 @@
-import Gallery from './Gallery.js';
-import { Profile } from './Gallery.js';
-
-export default function App() {
-    return (
-      <Profile />
-    );
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
 }
