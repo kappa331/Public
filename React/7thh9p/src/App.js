6f2070a0780c65eb105c972c313c1bb3df57fea5
kappa <wawata1qwerty@gmail.com>
2023-09-21 08:57:54 +0900

    finish tutorial 6

--- React/7thh9p/src/App.js
+++ React/7thh9p/src/App.js
@@ -1,84 +1,37 @@
-import { getImageUrl } from './utils.js';
-
-function Profile({person, size, career}){
-    return(
-        <>
-            <section className="profile">
-                <h2>{person.name}</h2>
-                <Avatar
-                    name={person.name}
-                    imageId={person.imageId}
-                    size={size}
-                />
-                <Career
-                    profession={career.profession}
-                    awards={career.awards}
-                    discoverd={career.discoverd}
-                />
-            </section>
-        </>
-    );
-}
-
-function Avatar({name, imageId, size}) {
-    return (
-        <img
-            className="avatar"
-            src={getImageUrl(imageId)}
-            alt={name}
-            width={size}
-            height={size}
-        />
-    );
-}
-
-function Career({profession, awards, discoverd}){
-    return (
-        <ul>
-            <li>
-                <b>Profession: </b>
-                {profession}
-            </li>
-            <li>
-                <b>Awards: {awards.count} </b>
-                ({awards.content})
-            </li>
-            <li>
-                <b>Discoverd: </b>
-                {discoverd}
-            </li>
-        </ul>
-    );
-}
-
-export default function Gallery() {
+function Item({ name, isPacked }) {
+  let itemContent = name;
+  if (isPacked) {
+      itemContent = (
+          <del>
+              {name + " ✔"}
+          </del>
+      );
+  }
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        person={{
-            name:'Maria Skłodowska-Curie',
-            imageId:'szV5sdG',
-        }}
-        size={70}
-        career={{
-            profession:'physicist and chemist',
-            awards:{count: '4', content:'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal'},
-            discoverd:'polonium (element)'
-        }}
-      />
-      <Profile
-        person={{
-            name:'Katsuko Saruhashi',
-            imageId:'YfeOqp2',
-        }}
-        size={70}
-        career={{
-            profession:'geochemist',
-            awards:{count: '2', content:'Miyake Prize for geochemistry, Tanaka Prize'},
-            discoverd:' a method for measuring carbon dioxide in seawater'
-        }}
-      />
-    </div>
+      <li className="item">
+          {itemContent}
+      </li>
+  );
+}
+
+export default function PackingList() {
+  return (
+      <section>
+          <h1>Sally Ride's Packing List</h1>
+          <ul>
+              <Item
+                  isPacked={true}
+                  name="Space suit"
+              />
+              <Item
+                  isPacked={true}
+                  name="Helemet with a golden leaf"
+              />
+              <Item
+                  isPacked={false}
+                  name="Photo of Tam"
+              />
+          </ul>
+      </section>
   );
 }
