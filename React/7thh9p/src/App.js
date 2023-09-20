6ad55507e2b963e4cce8b099984ccf694e327170
kappa <wawata1qwerty@gmail.com>
2023-09-20 10:38:21 +0900

    apply idea 2

--- React/7thh9p/src/App.js
+++ React/7thh9p/src/App.js
@@ -30,24 +30,29 @@ function Board({xIsNext, squares, onPlay}) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  function renderSquare(i) {
+    return (
+      <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+    );
+  }
+
+  const boardRows = [];
+  for(let row = 0; row < 3; row++){
+    const squaresInRow = [];
+    for(let col = 0; col < 3; col++){
+      squaresInRow.push(renderSquare(row * 3 + col));
+    }
+    boardRows.push(
+      <div className="board-row">
+        {squaresInRow}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardRows}
     </>
   );
 }
