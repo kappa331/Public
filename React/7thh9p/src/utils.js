565db3a50b271a6928556542d0bc4cfdca339280
kappa <wawata1qwerty@gmail.com>
2023-09-21 13:42:31 +0900

    finish tutorial 7

--- React/7thh9p/src/utils.js
+++ React/7thh9p/src/utils.js
@@ -1,9 +1,7 @@
-export function getImageUrl(imageId, size = 's') {
-    return (
-      'https://i.imgur.com/' +
-      imageId +
-      size +
-      '.jpg'
-    );
-  }
-  
+export function getImageUrl(person) {
+  return (
+    'https://i.imgur.com/' +
+    person.imageId +
+    's.jpg'
+  );
+}
--- React/learn/describing-the-ui/7_rendering-lists/App_2.js
+++ React/learn/describing-the-ui/7_rendering-lists/App_2.js
@@ -2,16 +2,19 @@ import {people} from './data.js';
 import {getImageUrl} from './utils.js';
 
 export default function List() {
-    const listItems = people.map(person =>
-        <li key={person.id}>
+    const chemists = people.filter(person =>
+        person.profession === 'chemist'
+    );
+    const listItems = chemists.map(person =>
+        <li>
             <img
                 src={getImageUrl(person)}
                 alt={person.name}
             />
             <p>
-                <b>{person.name}</b>
-                    {' ' + person.profession + ' '}
-                    known for {person.accomplishment}
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
             </p>
         </li>
     );
