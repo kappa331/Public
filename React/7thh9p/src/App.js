0818f06a9f58f2dbcfdb2f8e2835caaad73d43e1
kappa <wawata1qwerty@gmail.com>
2023-09-21 17:03:38 +0900

    finish tutorial 8

--- React/7thh9p/src/App.js
+++ React/7thh9p/src/App.js
@@ -1,19 +1,11 @@
-import {people} from './data.js';
-import {getImageUrl} from './utils.js';
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
 
-export default function List() {
-    const listItems = people.map(person =>
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}</b>
-                    {' ' + person.profession + ' '}
-                    known for {person.accomplishment}
-            </p>
-        </li>
-    );
-    return <ul>{listItems}</ul>;
+export default function TeaGathering() {
+  let cups = [];
+  for(let i = 1; i <= 12; i++) {
+      cups.push(<Cup key={i} guest={i} />);
+  }
+  return cups;
 }
